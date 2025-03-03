9ec4e611e319164812f85354ddbe76216e3051c3
Brian Schmitt <brianschmitt@gmail.com>
2025-03-03 14:35:02 -0500

    adding support for filter

--- plugin-flex-ts-template-v2/src/feature-library/canned-responses/custom-components/CannedResponsesCRM/CannedResponsesCRM.tsx
+++ plugin-flex-ts-template-v2/src/feature-library/canned-responses/custom-components/CannedResponsesCRM/CannedResponsesCRM.tsx
@@ -6,6 +6,7 @@ import { Column, Grid } from '@twilio-paste/core/grid';
 import { Stack } from '@twilio-paste/stack';
 import { ChatIcon } from '@twilio-paste/icons/esm/ChatIcon';
 import { Template, templates } from '@twilio/flex-ui';
+import { Input } from '@twilio-paste/core/input';
 
 import { CannedResponseCategories, ResponseCategory } from '../../types/CannedResponses';
 import Category from './Category';
@@ -16,6 +17,7 @@ const CannedResponsesCRM: React.FunctionComponent = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(false);
   const [responseCategories, setResponseCategories] = useState<undefined | CannedResponseCategories>(undefined);
+  const [searchText, setSearchText] = useState('');
 
   useEffect(() => {
     async function getResponses() {
@@ -32,6 +34,14 @@ const CannedResponsesCRM: React.FunctionComponent = () => {
     getResponses();
   }, []);
 
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  };
+
+  const filteredCategories = responseCategories?.categories.filter((category) =>
+    category.responses.some((response) => response.text.toLowerCase().includes(searchText.toLowerCase())),
+  );
+
   return (
     <Box as="div" padding="space50">
       <Stack orientation="horizontal" spacing="space30">
@@ -40,10 +50,11 @@ const CannedResponsesCRM: React.FunctionComponent = () => {
           <Template source={templates[StringTemplates.CannedResponses]} />
         </Text>
       </Stack>
+      <Input type="text" placeholder="Search responses..." value={searchText} onChange={handleSearchChange} />
       {isLoading && <SkeletonLoader />}
-      {Boolean(responseCategories) && !isLoading && (
+      {Boolean(filteredCategories) && !isLoading && (
         <>
-          {responseCategories?.categories.map((category: ResponseCategory) => (
+          {filteredCategories?.map((category: ResponseCategory) => (
             <Grid gutter="space30" vertical key={category.section}>
               <Column>
                 <Category {...category} />
